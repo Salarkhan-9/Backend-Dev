ed9c377465ac0058e405ca76132ddc8e916d579f
Muhammad Salar Khan Marwat <98265148+Salarkhan-9@users.noreply.github.com>
2024-03-21 18:10:56 +0000

    express get request

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get("twitter",(req,res)=>{
+app.get("/twitter",(req,res)=>{
     res.send("salar054")
+})
+app.get('/login',(req,res)=>{
+  res.send('<h1>Login here</h1>')
 })
 
 app.listen(port, () => {
